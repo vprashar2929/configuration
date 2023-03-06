a16ed8819529999b80873ceb60e16e29e237c639
Vibhu Prashar <vprashar@redhat.com>
2023-03-06 22:35:53 +0530

    Change order of comps in telemeter namespace

--- tests/ci_test.sh
+++ tests/ci_test.sh
@@ -121,7 +121,7 @@ telemeter(){
     oc create ns telemeter 1> /dev/null
     sleep 5
     oc apply --namespace telemeter -f telemeter-token-refersher-oidc-secret.yaml 1> /dev/null
-    comps=('memcached' 'nginx' 'memcached' 'token-refresher')
+    comps=('telemeter-server' 'telemeter-token-refresher' 'memcached' 'prometheus-remote-write-proxy' 'telemeter-server-canary')
     for comp in ${comps[*]}
     do
         oc process --param-file=telemeter.test.env -f ../resources/services/telemeter-template.yaml | oc apply --namespace telemeter --selector=app.kubernetes.io/name=$comp -f - 1> /dev/null

d0cb456da7326405fa63250f5c05bc255cad710a
Vibhu Prashar <vprashar@redhat.com>
2023-03-08 15:56:14 +0530

    Refactor previous changes

--- tests/ci/ci_test.sh
+++ tests/ci/ci_test.sh
@@ -5,18 +5,14 @@ set -o pipefail
 
 ARTIFACT_DIR="${ARTIFACT_DIR:-/tmp/artifacts}"
 INFO="INFO"
-ERROR="ERROR"
 log_info(){
     echo "$(date "+%Y-%m-%d %H:%M:%S") [$INFO] $1"
 }
-log_error(){
-    echo "$(date "+%Y-%m-%d %H:%M:%S") [$ERROR] $1"
-}
 check_status(){
-    resname=$1
+    res=$1
     namespace=$2
-    echo "oc rollout status $resname -n $namespace --timeout=5m"
-    oc rollout status $resname -n $namespace --timeout=5m || (must_gather "$ARTIFACT_DIR" && exit 1)
+    log_info "Checking status of $res inside $namespace"
+    oc rollout status $res -n $namespace --timeout=5m || (must_gather "$ARTIFACT_DIR" && exit 1)
 }
 crds(){
     log_info "Deploying CRD's on cluster"
@@ -29,7 +25,7 @@ minio(){
     log_info "Deploying resources inside minio namespace"
     oc create ns minio || true
     sleep 5
-    oc process -f minio-template.yaml -p MINIO_CPU_REQUEST=30m -p MINIO_CPU_LIMITS=50m -p MINIO_MEMORY_REQUEST=50Mi -p MINIO_MEMORY_LIMITS=100Mi --local -o yaml | sed -e 's/storage: [0-9].Gi/storage: 0.25Gi/g' | oc apply -n minio -f -
+    oc process -f ../minio-template.yaml -p MINIO_CPU_REQUEST=30m -p MINIO_CPU_LIMITS=50m -p MINIO_MEMORY_REQUEST=50Mi -p MINIO_MEMORY_LIMITS=100Mi --local -o yaml | sed -e 's/storage: [0-9].Gi/storage: 0.25Gi/g' | oc apply -n minio -f -
     sleep 5 
     check_status deployment/minio minio
 }
@@ -37,27 +33,23 @@ dex(){
     log_info "Deploying resources inside dex namespace"
     oc create ns dex || true
     sleep 5
-    oc process -f dex-template.yaml -p DEX_CPU_REQUEST=30m -p DEX_CPU_LIMITS=50m -p DEX_MEMORY_REQUEST=50Mi -p DEX_MEMORY_LIMITS=100Mi --local -o yaml | sed -e 's/storage: [0-9].Gi/storage: 0.25Gi/g' | oc apply -n dex -f -
+    oc process -f ../dex-template.yaml -p DEX_CPU_REQUEST=30m -p DEX_CPU_LIMITS=50m -p DEX_MEMORY_REQUEST=50Mi -p DEX_MEMORY_LIMITS=100Mi --local -o yaml | sed -e 's/storage: [0-9].Gi/storage: 0.25Gi/g' | oc apply -n dex -f -
     sleep 5
     check_status deployment/dex dex
-}
-teardown(){
-    log_info "Teardown started"
-    oc delete ns minio dex observatorium observatorium-metrics telemeter 1> /dev/null
 }
 observatorium_metrics(){
     log_info "Deploying resources inside observatorium-metrics namespace"
     oc create ns observatorium-metrics || true
     sleep 5
-    oc process -f observatorium-metrics-thanos-objectstorage-secret-template.yaml | oc apply --namespace observatorium-metrics -f - 
-    oc apply -f observatorium-alertmanager-config-secret.yaml --namespace observatorium-metrics
-    oc apply -f observatorium-cluster-role.yaml
-    oc apply -f observatorium-cluster-role-binding.yaml
-    oc apply --namespace observatorium-metrics -f observatorium-service-account.yaml
-    oc process --param-file=observatorium-metrics.ci.env -f ../resources/services/observatorium-metrics-template.yaml | oc apply --namespace observatorium-metrics -f - 
+    oc process -f ../observatorium-metrics-thanos-objectstorage-secret-template.yaml | oc apply --namespace observatorium-metrics -f - 
+    oc apply -f ../observatorium-alertmanager-config-secret.yaml --namespace observatorium-metrics
+    oc apply -f ../observatorium-cluster-role.yaml
+    oc apply -f ../observatorium-cluster-role-binding.yaml
+    oc apply --namespace observatorium-metrics -f ../observatorium-service-account.yaml
+    oc process --param-file=observatorium-metrics.ci.env -f ../../resources/services/observatorium-metrics-template.yaml | oc apply --namespace observatorium-metrics -f - 
     sleep 5
-    ress=$(oc get statefulsets -o name -n observatorium-metrics ; oc get deployments -o name -n observatorium-metrics)
-    for res in $ress
+    resources=$(oc get statefulsets -o name -n observatorium-metrics ; oc get deployments -o name -n observatorium-metrics)
+    for res in $resources
     do
         check_status $res observatorium-metrics
     done
@@ -66,12 +58,12 @@ observatorium(){
     log_info "Deploying resources inside observatorium namespace"
     oc create ns observatorium || true
     sleep 5
-    oc apply -f observatorium-rules-objstore-secret.yaml --namespace observatorium 
-    oc apply -f observatorium-rhobs-tenant-secret.yaml --namespace observatorium 
-    oc process --param-file=observatorium.test.ci.env -f ../resources/services/observatorium-template.yaml | oc apply --namespace observatorium -f -
+    oc apply -f ../observatorium-rules-objstore-secret.yaml --namespace observatorium 
+    oc apply -f ../observatorium-rhobs-tenant-secret.yaml --namespace observatorium 
+    oc process --param-file=observatorium.test.ci.env -f ../../resources/services/observatorium-template.yaml | oc apply --namespace observatorium -f -
     sleep 5
-    ress=$(oc get statefulsets -o name -n observatorium ; oc get deployments -o name -n observatorium)
-    for res in $ress
+    resources=$(oc get statefulsets -o name -n observatorium ; oc get deployments -o name -n observatorium)
+    for res in $resources
     do
         check_status $res observatorium
     done
@@ -81,21 +73,21 @@ telemeter(){
     log_info "Deploying resources inside telemeter namespace"
     oc create ns telemeter || true
     sleep 5
-    oc apply --namespace telemeter -f telemeter-token-refersher-oidc-secret.yaml 
-    oc process --param-file=telemeter.ci.env -f ../resources/services/telemeter-template.yaml | oc apply --namespace telemeter  -f - 
+    oc apply --namespace telemeter -f ../telemeter-token-refersher-oidc-secret.yaml 
+    oc process --param-file=telemeter.ci.env -f ../../resources/services/telemeter-template.yaml | oc apply --namespace telemeter  -f - 
     sleep 5
-    ress=$(oc get statefulsets -o name -n telemeter ; oc get deployments -o name -n telemeter)
-    for res in $ress
+    resources=$(oc get statefulsets -o name -n telemeter ; oc get deployments -o name -n telemeter)
+    for res in $resources
     do
         check_status $res telemeter
     done
 }
 run_test(){
-    oc apply -n observatorium -f secret.yaml
-    oc apply -n observatorium -f tenant.yaml
+    oc apply -n observatorium -f test-tenant.yaml
+    oc apply -n observatorium -f rbac.yaml
     oc rollout restart deployment/observatorium-observatorium-api -n observatorium
     check_status deployment/observatorium-observatorium-api observatorium
-    oc apply -n observatorium -f job.yaml
+    oc apply -n observatorium -f observatorium-up-metrics.yaml
     oc wait --for=condition=complete --timeout=5m -n observatorium job/observatorium-up-metrics || (must_gather "$ARTIFACT_DIR" && exit 1)
 }
 must_gather() {
